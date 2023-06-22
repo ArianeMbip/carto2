f867181f930e45993cb2989efe6542c5d8971f34
MADI François <francois.madi@orange.com>
2023-06-22 17:07:35 +0100

    fixe deployment helm chart

--- ApiCartobani/src/ApiCartobani/Extensions/Services/InfrastructureServiceExtension.cs
+++ ApiCartobani/src/ApiCartobani/Extensions/Services/InfrastructureServiceExtension.cs
@@ -12,14 +12,14 @@ public static class ServiceRegistration
     public static void AddInfrastructure(this IServiceCollection services, IWebHostEnvironment env)
     {
         // DbContext -- Do Not Delete
-        var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
-        if(string.IsNullOrEmpty(connectionString))
-        {
-            // this makes local migrations easier to manage. feel free to refactor if desired.
-            connectionString = env.IsDevelopment() 
-                ? "Data Source=localhost,49713;Integrated Security=False;Database=dev_dockerconfig;User ID=SA;Password=#localDockerPassword#"
-                : throw new Exception("DB_CONNECTION_STRING environment variable is not set.");
-        }
+        // var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+        // if(string.IsNullOrEmpty(connectionString))
+        // {
+        //     // this makes local migrations easier to manage. feel free to refactor if desired.
+        //     connectionString = env.IsDevelopment() 
+        //         ? "Data Source=localhost,49713;Integrated Security=False;Database=dev_dockerconfig;User ID=SA;Password=#localDockerPassword#"
+        //         : throw new Exception("DB_CONNECTION_STRING environment variable is not set.");
+        // }
 
         //services.AddDbContext<CartobaniDbContext>(options =>
         //    options.UseSqlServer(connectionString,
